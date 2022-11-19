83d777027a2450b37409dfe5fb66d63027555002
Likhita1212 <91164103+Likhita1212@users.noreply.github.com>
2022-11-18 20:06:25 -0500

    Add files via upload

---
+++ 110/roll-a-dice.py
@@ -0,0 +1,19 @@
+import random
+
+dice = random.randint(1,6)
+print("WELCOME TO THE DICE GAME!!!!!!!!")
+print("only use yes")
+print("Your number is...")
+print(dice)
+
+roll_again = input("Do you want to roll again? ")
+
+while roll_again == "yes" or "y" or "YES" or "Y":
+    print("Your number is...")
+    print(random.randint(1,6))
+    roll_again = input("Do you want to roll again? ")
+    if roll_again == "yes":
+        continue
+    else:
+        print("BYE!!!!!")
+        exit()
